95789e8e71c042abf07efd5138d8899eb7f29a08
Brian <wiinc1@hotmail.com>
2016-07-26 09:43:11 -0400

    Updated for project 1

--- 1.py
+++ 1.py
@@ -1,3 +1,21 @@
-shape("circle")
-shapesize(5,4,1)
-fillcolor("white")
+import turtle
+
+def draw_square(turtle_up):
+    turtle_up.color("grey")
+    for i in range(1,5):
+        turtle_up.forward(100)
+        turtle_up.right(80)
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor ("black")
+    brian = turtle.Turtle()
+    brian.shape("turtle")
+    brian.speed(25)
+    for i in range(1,26):
+        draw_square(brian)
+        brian.right(10)
+
+    window.exitonclick()
+
+draw_art()
